8fb7ed2f6de5da02de84366a97b9ea3706c65b8f
Andre Malan <raden.andre@gmail.com>
2014-08-16 12:45:22 -0400

    new and old players can be created

--- app/routes/login.js
+++ app/routes/login.js
@@ -4,10 +4,32 @@ export default Ember.Route.extend({
 
   actions: {
     login: function(){
+      var _this = this;
+      var controller = this.get('controller');
       var auth = this.get('auth');
-        auth.login('facebook', {
-        rememberMe: true
-      });
+      auth.login().then(function(user){
+        console.log('success auth in the then');
+        _this.store.find('player', user.uid).then(
+          function(theUser){
+          _this.auth.set('user', theUser);
+          console.log('old user set successfully', user);
+        },
+        function(error){
+          console.log('no user to find');
+          var newPlayer = _this.store.createRecord('player', {
+            id: user.uid,
+            name: user.displayName,
+            email: user.thirdPartyUserData.email
+          });
+
+          console.log('new player created', newPlayer);
+
+          newPlayer.save().then(function(player){
+            _this.auth.set('user', player);
+            console.log('user set successfully', user);
+          }, function(e){console.log('something went wrong', e)});
+        });
+      }, function(error){debugger;});
     }
   }
 });
