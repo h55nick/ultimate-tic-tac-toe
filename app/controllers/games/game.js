7c52b860193a1b72dcb5959cf1c27b15402bfb4c
Andre Malan <raden.andre@gmail.com>
2014-08-20 23:19:31 -0400

    setting players based on the game instead of the controller

--- app/controllers/games/game.js
+++ app/controllers/games/game.js
@@ -1,27 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init: function(){
-    var player1 = Ember.Object.create({
-        id: '123',
-        type: 1,
-        name: 'player1 (Nick)'
-      });
-    var player2 = Ember.Object.create({
-        id: '555',
-        type: 2,
-        name: 'player2 (Andre)'
-      });
-    this.set('player1',player1);
-    this.set('player2',player2);
-    this.set('currentPlayer', player1);
-
-  },
 
   game: Ember.computed.alias('content'),
-  player1: null,
-  player2: null,
-  currentPlayer: null,
+  player1: Ember.computed.alias('content.player1'),
+  player2: Ember.computed.alias('content.player2'),
+  currentPlayer: Ember.computed.alias('content.currentPlayer'),
   setPlayer1: false,
 
   actions: {
